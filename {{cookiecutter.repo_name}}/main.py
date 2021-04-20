d15dd1844846817d8c2fa360276bff6e3a66ca84
xuchaofeng <xuchaofeng-jk@360shuke.com>
2021-04-20 16:54:44 +0800

    fix isort

--- {{cookiecutter.repo_name}}/main.py
+++ {{cookiecutter.repo_name}}/main.py
@@ -17,5 +17,5 @@ app.add_middleware(
     allow_credentials=True,
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, access_log=True)
