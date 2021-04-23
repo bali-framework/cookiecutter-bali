ed56fcccb9cfa61befafaa61a7d3624c024279e6
Ed-XCF <m.tofu@qq.com>
2021-04-23 19:12:01 +0800

    support locale

--- {{cookiecutter.repo_name}}/conf.py
+++ {{cookiecutter.repo_name}}/conf.py
@@ -1,3 +1,5 @@
+import locale
+
 from bali.db import db
 from pydantic.env_settings import BaseSettings
 
@@ -8,8 +10,12 @@ class Settings(BaseSettings):
         env_file = '.env'
 
     TITLE: str = "{{cookiecutter.repo_name}}"
+    LOCALE: str
     DATABASE: str
+    RPC_PORT: int
+    RPC_THREAD_POOL_SIZE: int
 
 
 settings = Settings()
 db.connect(settings.DATABASE)
+locale.setlocale(locale.LC_ALL, settings.LOCALE)
--- {{cookiecutter.repo_name}}/services/rpc/service.py
+++ {{cookiecutter.repo_name}}/services/rpc/service.py
@@ -4,6 +4,7 @@ import grpc
 from bali.interceptors import ProcessInterceptor
 from loguru import logger
 
+from conf import settings
 from services.rpc import {{cookiecutter.repo_name}}_pb2 as pb2, {{cookiecutter.repo_name}}_pb2_grpc as pb2_grpc
 
 
@@ -12,15 +13,14 @@ class {{cookiecutter.repo_name|title}}Service(pb2_grpc.{{cookiecutter.repo_name|
 
 
 def serve():
-    port = 9080
     server = grpc.server(
-        futures.ThreadPoolExecutor(max_workers=10),
+        futures.ThreadPoolExecutor(max_workers=settings.RPC_THREAD_POOL_SIZE),
         interceptors=[ProcessInterceptor()],
     )
     pb2_grpc.add_{{cookiecutter.repo_name|title}}ServiceServicer_to_server({{cookiecutter.repo_name|title}}Service(), server)
-    server.add_insecure_port(f'[::]:{port}')
+    server.add_insecure_port(f'[::]:{settings.RPC_PORT}')
     server.start()
-    logger.info("Service started on port: {}", port)
+    logger.info("Service started on port: {}", settings.RPC_PORT)
     server.wait_for_termination()
 
 
