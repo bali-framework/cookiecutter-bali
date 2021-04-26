57f5f2a4cdafdbe4678b3a7c14c3fefe531f2d59
Ed-XCF <m.tofu@qq.com>
2021-04-26 10:19:56 +0800

    add helpers

---
+++ {{cookiecutter.repo_name}}/helpers/base_mq_producer.py
@@ -0,0 +1,19 @@
+from mq_http_sdk.mq_client import MQClient
+from mq_http_sdk.mq_producer import TopicMessage
+
+from conf import settings
+
+
+class Producer:
+    instance: str
+    topic: str
+
+    def __init__(self):
+        client = MQClient(
+            settings.MQ_HOST, settings.MQ_ACCESS_ID, settings.MQ_ACCESS_KEY
+        )
+        self.producer = client.get_producer(self.instance, self.topic)
+
+    def publish(self, msg: str, tag: str = None) -> None:
+        msg_ = TopicMessage(msg, tag)
+        self.producer.publish_message(msg_)
