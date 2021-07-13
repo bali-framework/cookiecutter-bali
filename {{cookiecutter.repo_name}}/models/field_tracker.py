e1f753b9df8e2f158a51a460bf7d6be720b26823
Ed-XCF <m.tofu@qq.com>
2021-07-13 10:31:50 +0800

    import datetime

--- {{cookiecutter.repo_name}}/models/field_tracker.py
+++ {{cookiecutter.repo_name}}/models/field_tracker.py
@@ -1,9 +1,10 @@
 import datetime
 import decimal
 import enum
-import uuid
+from uuid import UUID
 
-from bali.db import db
+from bali.db import AwareDateTime, db
+from bali.utils import timezone
 from loguru import logger
 from sqlalchemy import JSON, Column, Integer, String
 from sqlalchemy.event import listens_for
@@ -13,9 +14,13 @@ __all__ = ["FieldTracker"]
 
 
 class FieldTracker(db.BaseModel):
+    is_active = None
     id = Column(Integer, primary_key=True, autoincrement=True)
+    created_time = Column(AwareDateTime, default=timezone.now)
+    updated_time = Column(AwareDateTime, default=timezone.now, onupdate=timezone.now)
+
     table = Column(String(64), nullable=False)
-    uuid = Column(String(32))
+    row = Column(String(32), nullable=False)
     field = Column(String(64), nullable=False)
     old_value = Column(JSON)
     new_value = Column(JSON)
@@ -27,13 +32,18 @@ class FieldTracker(db.BaseModel):
             @listens_for(i, "set")
             @logger.catch
             def create(target, value, oldvalue, initiator):
-                if oldvalue is symbol("NO_VALUE"):
+                if any(
+                    [
+                        oldvalue is symbol("NO_VALUE"),
+                        value == oldvalue,
+                    ]
+                ):
                     return value
 
                 db.session.add(
                     cls(
                         table=target.__tablename__,
-                        uuid=target.uuid,
+                        row=target.id,
                         field=initiator.key,
                         old_value=_convert(oldvalue),
                         new_value=_convert(value),
@@ -58,7 +68,9 @@ def _convert(value):
         return r
     elif isinstance(value, enum.Enum):
         return value.name
-    elif isinstance(value, (decimal.Decimal, uuid.UUID)):
+    elif isinstance(value, decimal.Decimal):
         return str(value)
+    elif isinstance(value, UUID):
+        return value.hex
     else:
         return value
