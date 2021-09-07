a4d2b81da00739abbc1658960344ef79ed3ab2f6
Ed-XCF <m.tofu@qq.com>
2021-09-07 17:38:50 +0800

    better BUSINESS_KEY_PATH

--- {{cookiecutter.repo_name}}/consts.py
+++ {{cookiecutter.repo_name}}/consts.py
@@ -1,4 +1,5 @@
 ALWAYS_EXCLUDE = {"id", "is_active"}
 EXCLUDE_FOR_API = {*ALWAYS_EXCLUDE, "{{cookiecutter.business_key}}", "created_time", "updated_time"}
 BUSINESS_KEY_PATH = "/{{{cookiecutter.business_key}}}"
+BUSINESS_KEY_PATH = "/{}".format("{{cookiecutter.business_key}}")
 EMPTY_PATH = ""
