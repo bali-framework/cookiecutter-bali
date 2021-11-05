995ce6410a871bef6501e6ef63d0029e763e3cc5
Ed-XCF <m.tofu@qq.com>
2021-11-05 18:30:43 +0800

    mq support retry

--- {{cookiecutter.repo_name}}/helpers/base_mq_topic.py
+++ {{cookiecutter.repo_name}}/helpers/base_mq_topic.py
@@ -4,6 +4,7 @@ from loguru import logger
 from mq_http_sdk.mq_client import MQClient
 from mq_http_sdk.mq_producer import TopicMessage
 from pydantic import BaseModel
+from tenacity import retry, stop_after_attempt, stop_after_delay
 
 from conf import settings
 
@@ -22,13 +23,21 @@ class BaseTopic:
         )
         self.producer = client.get_producer(settings.MQ_INSTANCE, self.topic)
 
-    def publish(self, msg: Any, *, tag: str = None, fail_silently: bool = False) -> None:
+    @retry(stop=(stop_after_delay(5) | stop_after_attempt(3)))
+    def _publish(self, topic_message):
+        self.producer.publish_message(topic_message)
+
+    def publish(
+        self, msg: Any, *, tag: str = None, fail_silently: bool = False
+    ) -> None:
         data = self.Schema.parse_obj(msg)
         try:
-            topic_message = TopicMessage(data.json(), tag or "")
-            self.producer.publish_message(topic_message)
+            topic_message = TopicMessage(data.json(), (tag or "").lower())
+            self._publish(topic_message)
         except Exception as e:
             if not fail_silently:
                 raise
 
             logger.warning("Publish msg to {} failed: {}", self.topic, repr(e))
+        else:
+            logger.info("Publish msg to {} success", self.topic)
